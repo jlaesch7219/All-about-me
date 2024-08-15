6719096786238b277ee15a73634353c38e214d33
Jackson Laesch <jlaesch7219@stu.d214.org>
2024-08-15 09:18:58 -0500

    Initial Commit

---
+++ All about me/All_about_meApp.swift
@@ -0,0 +1,17 @@
+//
+//  All_about_meApp.swift
+//  All about me
+//
+//  Created by Jackson A. Laesch on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct All_about_meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ All about me/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  All about me
+//
+//  Created by Jackson A. Laesch on 8/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
