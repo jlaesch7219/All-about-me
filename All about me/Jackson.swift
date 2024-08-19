d0c3316ab42fce4568ccb6f885c026dcedd1e7d7
Jackson Laesch <jlaesch7219@stu.d214.org>
2024-08-19 09:34:23 -0500

    s

--- All about me/Jackson.swift
+++ All about me/Jackson.swift
@@ -3,8 +3,17 @@ import SwiftUI
 struct Jackson: View {
     var body: some View {
         VStack {
+            Text("All About Me")
+                .fontWeight(.black)
+                .font(.system(size: 35))
+                .underline()
             Text("Jackson")
+                .fontWeight(.heavy)
+                .font(.system(size: 65))
             
         }
     }
+}
+#Preview {
+    Jackson()
 }
