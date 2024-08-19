7769040ffcc49726bc860bf1bfe6f549b27151ed
Jackson Laesch <jlaesch7219@stu.d214.org>
2024-08-19 09:17:39 -0500

    s

--- All about me/Jackson.swift
+++ All about me/Jackson.swift
@@ -3,9 +3,7 @@ import SwiftUI
 struct Jackson: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+            Text("Jackson")
             
         }
     }
