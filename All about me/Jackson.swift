6536193a18ea4e8549329f6b6f6ef21ff5d6722b
Jackson Laesch <jlaesch7219@stu.d214.org>
2024-08-21 09:19:20 -0500

    dw

--- All about me/Jackson.swift
+++ All about me/Jackson.swift
@@ -16,10 +16,35 @@ struct Jackson: View {
             Text("   2027 🎓")
                 .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
                 .font(.system(size: 30))
+           Divider()
+          
+            Text("2 Truths and a Lie")
+                .fontWeight(.black)
+                .font(.system(size: 35))
+                .underline()
+                    Spacer()
+                .frame(height: 50)
+            Text("• I have never left the Country")
+                .fontWeight(.semibold)
+                .font(.system(size: 29))
+            Spacer()
+        .frame(height: 30)
+            Text("• I have broken my arm twice")
+                .fontWeight(.semibold)
+                .font(.system(size: 29))
+            Spacer()
+        .frame(height: 30)
+            Text("• I have lived in Illinois my whole life")
+                .fontWeight(.semibold)
+                .font(.system(size: 29))
+                }
+            }
             
         }
-    }
-}
+    
+
+
+
 #Preview {
     Jackson()
 }
